3e72d57c902c8a8bf5762b1b79fd76d6d3de7ee2
uxffm <35499917+uxffm@users.noreply.github.com>
2025-04-11 10:57:20 +0200

    yes

--- dist/chunks/content-modules_D4m7JORd.mjs
+++
@@ -1,5 +0,0 @@
-const contentModules = new Map([
-["src/content/pages/german/elements.mdx", () => import('./elements__mPZirEu.mjs')],
-["src/content/pages/english/elements.mdx", () => import('./elements_cHNQBT3I.mjs')]]);
-
-export { contentModules as default };
--- dist/chunks/menu.de_BaTWsnYV.mjs
+++
@@ -1,8 +0,0 @@
-const main = [{"name":"Seiten","url":"","hasChildren":true,"children":[{"name":"Startseite","url":"/"},{"name":"Preise","url":"/pricing"},{"name":"Team","url":"/about"},{"name":"Features","url":"/features"},{"name":"Erfahrungen","url":"/review"},{"name":"Blog","url":"/blog"},{"name":"Kontakt","url":"/contact"},{"name":"Cambios","url":"/changelog"},{"name":"Demo","url":"/demo"},{"name":"Integraciones","url":"/integration"},{"name":"Términos","url":"/terms"},{"name":"Privacidad","url":"/privacy-policy"},{"name":"404","url":"/404"}]},{"name":"Características","url":"/features"}];
-const footer = [{"title":"Producto","children":[{"name":"Resumen","url":"/"},{"name":"Características","url":"/features"},{"name":"Soluciones","url":"/"}]},{"title":"Compañía","children":[{"name":"Acerca","url":"/about"},{"name":"Carreras","url":"/about"},{"name":"Prensa","url":"/about"}]},{"title":"Recursos","children":[{"name":"Blog","url":"/blog"},{"name":"Boletín","url":"/blog"},{"name":"Evento","url":"/about"}]},{"title":"Social","children":[{"name":"LinkedIn","url":"https://www.linkedin.com/"},{"name":"Facebook","url":"https://www.facebook.com/"},{"name":"Twitter","url":"https://twitter.com/"}]},{"title":"Legal","children":[{"name":"Términos","url":"/terms"},{"name":"Privacidad","url":"/privacy-policy"},{"name":"Cookies","url":"/"}]}];
-const menu_de = {
-  main,
-  footer,
-};
-
-export { menu_de as default, footer, main };
