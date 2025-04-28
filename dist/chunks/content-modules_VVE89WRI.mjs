9047b4bd7718f66494ced1ed207fb025d4583b55
uxffm <35499917+uxffm@users.noreply.github.com>
2025-04-28 11:08:52 +0200

    yew

--- dist/chunks/content-modules_VVE89WRI.mjs
+++
@@ -1,5 +0,0 @@
-const contentModules = new Map([
-["src/content/pages/german/elements.mdx", () => import('./elements_BfTysAtF.mjs')],
-["src/content/pages/english/elements.mdx", () => import('./elements_G0NI4Qax.mjs')]]);
-
-export { contentModules as default };
